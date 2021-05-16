cc73f8d29b1d46c56521a7cbb73af04fa7f88d25
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2021-05-15 20:18:23 -0500

    Round 709

---
+++ Round709/A.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+
+int main(){
+
+    int t; 
+    scanf("%d", &t);
+    while(t--){
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a * b);
+    }
+    return 0;
+}
